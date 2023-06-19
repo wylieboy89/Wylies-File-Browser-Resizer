dd22dd75d5484dffcb96ea4b24cce15f39fdd08e
wylieboy89 <135457658+wylieboy89@users.noreply.github.com>
2023-06-19 15:03:50 +1000

    Add files via upload

---
+++ module.js
@@ -0,0 +1,19 @@
+﻿Hooks.on('init', () => {
+  game.settings.register('Wylies-File-Browser-Resizer', 'maxHeight', {
+    name: 'Max Height',
+    hint: 'Set the maximum height of the filepicker directory in pixels.',
+    scope: 'client',
+    config: true,
+    default: 200,
+    type: Number,
+    onChange: value => {
+      document.documentElement.style.setProperty('--wfb-resizer-max-height', `${value}px`);
+    }
+  });
+});
+
+Hooks.on('ready', () => {
+  const maxHeight = game.settings.get('Wylies-File-Browser-Resizer', 'maxHeight');
+  document.documentElement.style.setProperty('--wfb-resizer-max-height', `${maxHeight}px`);
+});
+
